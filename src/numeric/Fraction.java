66a1c517b0ca85ec99518b754a141bdcac2ef054
ic238694 <41132457+ic238694@users.noreply.github.com>
2024-06-04 09:30:11 -0400

    Update Fraction.java

--- src/numeric/Fraction.java
+++ src/numeric/Fraction.java
@@ -6,7 +6,7 @@ package numeric;
 import java.math.BigInteger;
 
 /**
- * @author pr-20240604-4
+ * @author pr-20240604-12
  *
  */
 public final class Fraction extends Number implements Comparable<Fraction> {
